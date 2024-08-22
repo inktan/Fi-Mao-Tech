2453a8839a1f60fdbe3aeb0beff2e19a1f830614
inktan <30437424+inktan@users.noreply.github.com>
2024-08-22 15:45:26 +0800

    goa

--- vue-ai-map/src/router/index.js
+++ vue-ai-map/src/router/index.js
@@ -5,19 +5,19 @@ const router = createRouter({
     history: createWebHistory(
         import.meta.env.BASE_URL),
     routes: [{
-            path: '/',
-            name: 'home',
-            component: LngLatInfo
-        },
-        {
-            path: '/about',
-            name: 'about',
-            // route level code-splitting
-            // this generates a separate chunk (About.[hash].js) for this route
-            // which is lazy-loaded when the route is visited.
-            component: () =>
-                import ('@/views/LngLatInfo.vue')
-        }
+        path: '/',
+        name: 'home',
+        component: LngLatInfo
+    },
+    {
+        path: '/about',
+        name: 'about',
+        // route level code-splitting
+        // this generates a separate chunk (About.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import('@/views/LngLatInfo.vue')
+    }
     ]
 })
 
