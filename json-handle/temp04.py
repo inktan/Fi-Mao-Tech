6e162426abb91dcd57d43032c674b812a517f30e
inktan <mslneed@gmail.com>
2024-09-13 11:42:03 +0800

    goa

--- json-handle/temp04.py
+++ json-handle/temp04.py
@@ -4,8 +4,16 @@
 # if all_0:
 #     print('qwe')
         
+# lst = []  
+# result = len(set(lst)) == 1 and list(set(lst))[0] == 0  
+# print(result)  # 输出: True
+import pandas as pd  
 
+df = pd.read_csv(r'e:\work\sv_chenlong20240907\RoadPoints_50m_unique.csv')
 
-lst = []  
-result = len(set(lst)) == 1 and list(set(lst))[0] == 0  
-print(result)  # 输出: True
+filtered_df = df.loc[df['PointID'] == 9]  
+print(filtered_df.iloc[0,0])
+print(filtered_df.iloc[0,1])
+print(filtered_df.iloc[0,2])
+print(filtered_df.iloc[0,3])
+
