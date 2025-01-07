dbefe89dc86d645f8309a53c1c7b6107b29f5b30
inktan <30437424+inktan@users.noreply.github.com>
2025-01-07 15:04:06 +0800

    goa

---
+++ vue-fimotech/src/components/Example.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+// import HelloWorld from './components/HelloWorld.vue'
+import { useGlobalStore } from '@/store/globalStore'
+const globalStore = useGlobalStore()
+
+import { useRoute, useRouter } from 'vue-router';
+const router = useRouter();
+
+
+</script>
+
+<template>
+</template>
+
+<style lang="less"></style>
+
+
+
+
+
+
